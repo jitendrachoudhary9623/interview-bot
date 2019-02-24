38f4f1b6da9ee650b97a2a09adbd6c5d674c7259
Jitendr Choudhary <Jitendra93266@gmail.com>
2019-02-24 21:39:44 +0530

    Integrated Sentiment Analysis Module

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 import aiml
 import os
+from nlp import TextAnalyser
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 kernel = aiml.Kernel()
-
+sid = SentimentIntensityAnalyzer()
 def load_kern(forcereload):
 	if os.path.isfile("bot_brain.brn") and not forcereload:
 		kernel.bootstrap(brainFile= "bot_brain.brn")
@@ -17,29 +19,49 @@ app = Flask(__name__)
 def hello():
 	load_kern(False)
 	return render_template('chat.html')
-
+asked=[]
 @app.route("/ask", methods=['POST','GET'])
 def ask():
 	print("Hello")
 	message = str(request.form['messageText'])
 	print(message)
 	if message == "save":
-	    kernel.saveBrain("bot_brain.brn")
-	    return jsonify({"status":"ok", "answer":"Brain Saved"})
+	    	kernel.saveBrain("bot_brain.brn")
+	    	return jsonify({"status":"ok", "answer":"Brain Saved"})
 	elif message == "reload":
 		load_kern(True)
 		return jsonify({"status":"ok", "answer":"Brain Reloaded"})
 	elif message == "quit":
 		exit()
 		return jsonify({"status":"ok", "answer":"exit Thank You"})
-
-	# kernel now ready for use
 	else:
-		# while True:
 		bot_response = kernel.respond(message)
+		temp=bot_response
+		if temp in asked:
+			print("Already Asked")
+			bot_response=kernel.respond("NEXT QUESTION")
+			return jsonify({'status':'OK','answer':bot_response})
+		asked.append(temp)
 		print(bot_response)
 		# print bot_response
 		return jsonify({'status':'OK','answer':bot_response})
+
+@app.route('/sentiment',methods=['POST','GET'])
+def sentiments():
+	print("sentiment")
+	message = str(request.form['messageText'])
+	print(message)
+	scores = sid.polarity_scores(message)
+	if scores['compound'] > 0:
+		sentiment='Positive'
+	elif scores['compound']< 0:
+		sentiment='Negative'
+	else:
+		sentiment='Neutral'
+	return jsonify({'status':'OK','sentiment_positive':scores['pos'],
+'sentiment_negative':scores['neg'],
+'sentiment_neutral':scores['neu'],
+'overall_sentiment':sentiment})		
 
 if __name__ == "__main__":
     app.run(debug=True)
